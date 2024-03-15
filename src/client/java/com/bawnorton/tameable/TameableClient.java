143c70d4c761d669bcda28c9729f323b218f99a3
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2024-03-15 13:38:04 -0700

    Refactor

--- src/client/java/com/bawnorton/tameable/TameableClient.java
+++
@@ -1,9 +0,0 @@
-package com.bawnorton.tameable;
-
-import net.fabricmc.api.ClientModInitializer;
-
-public class TameableClient implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-	}
-}
