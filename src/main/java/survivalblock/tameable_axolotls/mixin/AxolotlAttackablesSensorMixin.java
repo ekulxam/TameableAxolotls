f77f8bc55d4457ab44b495cf2586dfa5b1ae1dec
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2024-03-17 15:29:57 -0700

    Redid AxolotlAttackablesSensorMixin

--- src/main/java/survivalblock/tameable_axolotls/mixin/AxolotlAttackablesSensorMixin.java
+++ src/main/java/survivalblock/tameable_axolotls/mixin/AxolotlAttackablesSensorMixin.java
@@ -1,5 +1,6 @@
 package survivalblock.tameable_axolotls.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.brain.sensor.AxolotlAttackablesSensor;
 import net.minecraft.entity.passive.AxolotlEntity;
@@ -9,20 +10,23 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(AxolotlAttackablesSensor.class)
+@Mixin(value = AxolotlAttackablesSensor.class, priority = 1500)
 public class AxolotlAttackablesSensorMixin {
 
-    @Inject(method = "canHunt", at = @At(value = "RETURN"), cancellable = true)
-    public void allowAttackingAll(LivingEntity entity, LivingEntity target, CallbackInfoReturnable<Boolean> cir) {
-        if(entity instanceof AxolotlEntity axolotl && !axolotl.getWorld().isClient()){
-            if(((Object) axolotl) instanceof TameableEntity tameable && tameable.isTamed() && tameable.getOwner() != null){
-                if(tameable.getOwner().getAttacking() != null){
-                    cir.setReturnValue(tameable.getOwner().getAttacking() == target);
-                } else if(tameable.getOwner().getAttacker() != null){
-                    cir.setReturnValue(tameable.getOwner().getAttacker() == target);
-                }
-            }
+    @ModifyReturnValue(method = "canHunt", at = @At(value = "TAIL"))
+    public boolean allowAttackingAll(boolean original, LivingEntity entity, LivingEntity target) {
+        if (!(entity instanceof AxolotlEntity axolotl) || axolotl.getWorld().isClient()) {
+            return original;
+        }
+        if (!(((Object) axolotl) instanceof TameableEntity tameable) || !tameable.isTamed() || tameable.getOwner() == null) {
+            return original;
+        }
+        if(tameable.getOwner().getAttacking() != null){
+            return tameable.getOwner().getAttacking().equals(target);
+        } else if(tameable.getOwner().getAttacker() != null){
+            return tameable.getOwner().getAttacker().equals(target);
+        } else {
+            return original;
         }
     }
-
 }
