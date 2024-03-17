bf693433f99331c402fb0e689e57cf89dfb01579
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2024-03-16 21:19:13 -0700

    Redo iterator in PlayDeadTaskMixin
    Fixed mixin throwing a ConcurrentModificationException

--- src/main/java/survivalblock/tameable_axolotls/mixin/PlayDeadTaskMixin.java
+++ src/main/java/survivalblock/tameable_axolotls/mixin/PlayDeadTaskMixin.java
@@ -4,6 +4,7 @@ import net.minecraft.entity.ai.brain.MemoryModuleState;
 import net.minecraft.entity.ai.brain.MemoryModuleType;
 import net.minecraft.entity.ai.brain.task.MultiTickTask;
 import net.minecraft.entity.ai.brain.task.PlayDeadTask;
+import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
@@ -15,6 +16,8 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -28,11 +31,12 @@ public abstract class PlayDeadTaskMixin extends MultiTickTask<AxolotlEntity> {
 
     @Inject(method="run(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/entity/passive/AxolotlEntity;J)V", at = @At("TAIL"))
     private void addStrength(ServerWorld serverWorld, AxolotlEntity axolotlEntity, long l, CallbackInfo ci){
-        List<StatusEffectInstance> axolotlStatusEffectList = new java.util.ArrayList<>(List.copyOf(axolotlEntity.getStatusEffects()));
-        for(StatusEffectInstance thisStatusEffectInstance : axolotlStatusEffectList){
-            if(thisStatusEffectInstance.getEffectType().getCategory().equals(StatusEffectCategory.HARMFUL)){
-                axolotlStatusEffectList.remove(thisStatusEffectInstance);
-                axolotlEntity.removeStatusEffect(thisStatusEffectInstance.getEffectType());
+        List<StatusEffectInstance> axolotlStatusEffectList = new ArrayList<>(List.copyOf(axolotlEntity.getStatusEffects()));
+        for (Iterator<StatusEffectInstance> iterator = axolotlStatusEffectList.iterator(); iterator.hasNext(); ) {
+            StatusEffect statusEffectType = iterator.next().getEffectType();
+            if(statusEffectType.getCategory().equals(StatusEffectCategory.HARMFUL)){
+                axolotlEntity.removeStatusEffect(statusEffectType);
+                iterator.remove();
             }
         }
         axolotlEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 400, 1));
